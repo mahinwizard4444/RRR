fc444fa854bf5626d96f4db43d13173e47d77d66
Mufaz-TG <93363575+BXBotz2021@users.noreply.github.com>
2022-05-01 08:04:20 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,12 @@
-echo "Cloning Repository"
-git clone https://<pat>@github.com/BXBotz2021/Spider-Man.git
-cd Spider-Man
+if [ -z $UPSTREAM_REPO ]
+then
+  echo "Cloning main Repository"
+  git clone https://github.com/BXBotz2021/Spider-V2.git /Spider-V2
+else
+  echo "Cloning Custom Repo from $UPSTREAM_REPO "
+  git clone $UPSTREAM_REPO /Spider-V2
+fi
+cd /Spider-V2
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
