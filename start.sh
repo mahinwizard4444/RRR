a01882662a2156dd3548b353a51b7ab69b2a47cd
mahinwizard4444 <86041088+mahinwizard4444@users.noreply.github.com>
2022-05-05 22:34:41 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://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@github.com/mahinwizard4444/RRR.git /RRR
+  git clone https://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@github.com/BXBotz2021/Spider-Man.git /Spider-V2
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /RRR
+  git clone $UPSTREAM_REPO /Spider-V2
 fi
-cd /RRR
+cd /Spider-V2
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
